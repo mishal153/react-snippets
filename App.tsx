792ee1083a65301cf7bb7e68c668f08779dd8f36
mishal153 <mishalsang@gmail.com>
2022-05-12 01:09:35 +1000

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { useState } from 'react';
+import './style.css';
+import { Todos } from './Todos';
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, 'New Todo']);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
